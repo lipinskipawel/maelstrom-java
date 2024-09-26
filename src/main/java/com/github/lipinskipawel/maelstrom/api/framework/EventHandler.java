c9df1c19ca4a7b46ec007dd0da2190122e354d50
Pawel Lipinski <lipinskijob@gmail.com>
2024-09-26 07:29:50 +0200

    Add debug API

--- src/main/java/com/github/lipinskipawel/maelstrom/api/framework/EventHandler.java
+++ src/main/java/com/github/lipinskipawel/maelstrom/api/framework/EventHandler.java
@@ -29,6 +29,29 @@ public abstract class EventHandler<W extends BaseWorkload> {
         System.out.flush();
     }
 
+    /**
+     * Prints debug message to the standard error.
+     * In order to see debug messages start server with `--log-stderr` flag.
+     *
+     * @param debugMessage that will be printed
+     */
+    public void debug(String debugMessage) {
+        System.err.println(debugMessage);
+        System.err.flush();
+    }
+
+    /**
+     * Prints debug message to standard error with event. Internally `debugMessage` is calling formatted method with
+     * event as a argument.
+     * In order to see debug messages start server with `--log-stderr` flag.
+     *
+     * @param debugMessage that will be printed
+     * @param event        that will be formatted into the debug message
+     */
+    public void debug(String debugMessage, Event<?> event) {
+        debug(debugMessage.formatted(parse(event)));
+    }
+
     private String parse(Event<?> event) {
         return writeEvent(event);
     }
